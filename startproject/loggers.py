73035af88ec96deb856aa7e3a935c151090e7f95
Jakub Pocentek <jpocentek@gmail.com>
2015-12-05 18:32:16 +0100

    Loggers

---
+++ startproject/loggers.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'simple': {
+            'format': '[%(asctime)s %(module)s] %(levelname)s: %(message)s',
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple',
+        },
+        'main': {
+            'level': 'WARNING',
+            'class': 'logging.FileHandler',
+            'formatter': 'simple',
+            'filename': os.path.join(BASE_DIR, 'logs', 'django.log'),
+        },
+    },
+    'loggers': {
+        'default': {
+            'handlers': ['console', 'main',],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
